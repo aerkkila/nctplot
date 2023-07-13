83e0b59c3c0e694c245734bb395856e74f17d8d3
  <>
2023-07-13 14:50:49 +0300

    vähän selvennetty

--- executable/bash_completion.sh
+++ executable/bash_completion.sh
@@ -1,6 +1,5 @@
 _nctplot() {
     local last=' '
-    _init_completion
     COMPREPLY=($(compgen -o plusdirs -f -X '!*.nc' "${COMP_WORDS[COMP_CWORD]}"))
     if [ ${#COMPREPLY[@]} = 1 ]; then
 	[ -d "$COMPREPLY" ] && last=/
--- library/config.mk
+++ library/config.mk
@@ -1,4 +1,4 @@
-# shplib is used in drawing coastlines. Comment out or remove the following two lines if you don't have shplib installed.
+# shapelib/shplib is used in drawing coastlines. Comment out the following two lines if you don't have shapelib installed.
 HAVE_SHPLIB = -DHAVE_SHPLIB
 libraries += -lshp -lm
 
