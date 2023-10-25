650af4df20cc1c3141ebb30b95d0f3d99c278bbf
  <>
2023-10-25 08:29:39 +0300

    hyppykäskylle voidaan antaa myös päivämäärä

--- library/nctplot.c
+++ library/nctplot.c
@@ -743,10 +743,37 @@ static void multiply_zoom(Arg arg) {
 static void jump_to(Arg _) {
     printf("Enter a framemumber to jump to \033[K");
     fflush(stdout);
-    if(scanf("%i", &plt.znum) != 1)
+    int arg0, month=0, day=1, hour=0, minute=0;
+    float second=0;
+    switch (scanf("%d-%d-%d[ *]%d:%d:%f", &arg0, &month, &day, &hour, &minute, &second)) {
+	case 1: plt.znum = arg0; break; // user entered a frame number
+	case 0: break;
+	case -1: warn("scanf in %s", __func__); break;
+	default:
+	    if (plt.time0.d < 0)
+		return; // date entered but z-coordinate is not date
+	    struct tm tm = {
+		.tm_year = arg0-1900,
+		.tm_mon = month-1,
+		.tm_mday = day,
+		.tm_hour = hour,
+		.tm_min = minute,
+		.tm_sec = second,
+	    };
+	    time_t target_time = mktime(&tm);
+	    time_t current_time = nct_mktime(plt.zvar, NULL, &plt.time0, plt.znum).a.t;
+	    int move = (target_time - current_time)*1000 / nct_get_interval_ms(plt.time0.d);
+	    // TODO milliseconds
+	    plt.znum += move;
+	    break;
+    }
+    if (plt.znum < 0)
 	plt.znum = 0;
+    else if (plt.znum >= plt.zvar->len)
+	plt.znum = plt.zvar->len-1;
     lines_echoed--;
     fflush(stdout);
+    call_redraw = 1;
 }
 
 static void pending_var_dec(Arg _) {
