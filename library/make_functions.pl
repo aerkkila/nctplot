390d374769ea0d12dc950f3ccc39723d76e7df37
  <>
2024-04-26 16:51:49 +0300

    make_functions.pl yksinkertaistettu ja functions.c on siten erilainen

--- library/make_functions.pl
+++ library/make_functions.pl
@@ -1,143 +1,120 @@
 #!/bin/env perl
 use warnings;
-$fname0   = 'functions.in.c';
-@nctypes  = ('NC_BYTE', 'NC_UBYTE', 'NC_CHAR', 'NC_SHORT', 'NC_USHORT', 'NC_INT', 'NC_UINT',
-	     'NC_INT64', 'NC_UINT64', 'NC_FLOAT', 'NC_DOUBLE');
-@formats  = ('hhi', 'hhu', 'c', 'hi', 'hu', 'i', 'u', 'lli', 'llu', 'f', 'lf');
+@enumtypes  = ('NC_BYTE', 'NC_UBYTE', 'NC_CHAR', 'NC_SHORT', 'NC_USHORT', 'NC_INT', 'NC_UINT',
+	     'NC_INT64', 'NC_UINT64', 'NC_FLOAT', 'NC_DOUBLE', 'NC_STRING');
+@formats  = ('hhi', 'hhu', 'c', 'hi', 'hu', 'i', 'u', 'lli', 'llu', 'f', 'lf', 's');
 @ctypes   = ('char', 'unsigned char', 'char', 'short', 'unsigned short', 'int', 'unsigned',
-             'long long', 'long long unsigned', 'float', 'double');
+             'long long', 'long long unsigned', 'float', 'double', 'char*');
 @uctypes  = ('unsigned char', 'unsigned char', 'unsigned char', 'unsigned short', 'unsigned short', 'unsigned', 'unsigned',
              'long long unsigned', 'long long unsigned', 'float', 'double');
-
-$quiet = grep(/^-q$/, @ARGV);
 
 sub make_wrapper_function {
     ($type, $name, $args) = @_;
 
     # create a function array
-    print my_out_wrappers "static $type (*__$name"."[])($args) = {\n";
+    $ntypes = @enumtypes - 1 + $stringalso;
+    if (not $type =~ /^static($|\s)/) {
+	print file_out "static "; }
+    print file_out "$type (*__$name"."[])($args) = {\n";
     for(my $j=0; $j<$ntypes; $j++) {
-	print my_out_wrappers "    [$nctypes[$j]] = $name"."_$nctypes[$j],\n"; }
-    print my_out_wrappers "};\n";
+	print file_out "    [$enumtypes[$j]] = $name"."_$enumtypes[$j],\n"; }
+    print file_out "};\n";
 
     # create a wrapper function
-    print my_out_wrappers "$type $name("; # now: type foo(
+    print file_out "$type $name("; # now: type foo(
     my @arg = split(/,/, $args);
     my $nargs = @arg;
     my $nargs1 = $nargs - 1;
-    our $hasvar = $nargs && ($arg[0] =~ /^(const\s+)?(nct_var)|(nct_att)\*/);
+    our $hasvar = $nargs && ($arg[0] =~ /^([^,]*\s+)?(nct_var)|(nct_att)\*/);
     if ($hasvar) {
         for(my $i=0; $i<$nargs-1; $i++) {
-            print my_out_wrappers "$arg[$i] _$i, "; }
-        print my_out_wrappers "$arg[$nargs1] _$nargs1) {\n"; # now: type foo(type0 _0, type1 _1, type_n, _n) {
-        print my_out_wrappers "    return __$name"."[_0->dtype](";
+            print file_out "$arg[$i] _$i, "; }
+        print file_out "$arg[$nargs1] _$nargs1) {\n"; # now: type foo(type0 _0, type1 _1, type_n, _n) {
+        print file_out "    return __$name"."[_0->dtype](";
         for($i=0; $i<$nargs-1; $i++) {
-            print my_out_wrappers "_$i, "; }
-        print my_out_wrappers "_$nargs1);\n}\n";
+            print file_out "_$i, "; }
+        print file_out "_$nargs1);\n}\n";
     }
     else {
-        print my_out_wrappers "nc_type nctype";
+        print file_out "nc_type nctype";
         for(my $i=0; $i<$nargs-1; $i++) {
-            print my_out_wrappers ", $arg[$i] _$i"; }
+            print file_out ", $arg[$i] _$i"; }
 	if ($nargs) {
-	    print my_out_wrappers ", $arg[$nargs1] _$nargs1"; }
-        print my_out_wrappers ") {\n"; # now: type foo(type0 _0, type1 _1, type_n, _n) {
-        print my_out_wrappers "    return __$name"."[nctype](";
+	    print file_out ", $arg[$nargs1] _$nargs1"; }
+        print file_out ") {\n"; # now: type foo(type0 _0, type1 _1, type_n, _n) {
+        print file_out "    return __$name"."[nctype](";
         if ($nargs) {
-            print my_out_wrappers "_0"; }
+            print file_out "_0"; }
         for(my $i=1; $i<$nargs; $i++) {
-            print my_out_wrappers ", _$i"; }
-        print my_out_wrappers ");\n}\n";
+            print file_out ", _$i"; }
+        print file_out ");\n}\n";
     }
 }
 
-$fname = $fname0;
-$fname =~ s/\.in\.c/.c/;
-open out1, ">$fname"; # functions.c
-print out1 "#include <string.h>\n#include <nctietue3.h>\n#include <math.h>\n\n";
+sub main {
+    open file_in, "<functions.in.c" or die;
+    open file_out, ">functions.c";
 
-open file_in, "<$fname0"; # functions.in.c
-
-# Skip a possible comment at the beginning.
-$line = <file_in>;
-$startpos = 0;
-if ($line =~ /^\/\*/) {
-    while(1) {
-	if ($line =~ /\*\// or eof file_in) {
-	    $startpos = tell file_in;
-	    last;
-	}
+    # Copy only once until @startperl.
+    $line = <file_in>;
+    while (not $line =~ /^\@startperl\s*.*/) { #*
+	print file_out $line;
 	$line = <file_in>;
     }
-}
+    $startpos = tell file_in;
 
-# Skip empty lines at the beginning.
-while(<file_in>) {
-    if (/^\s*$/) { # fix syntax highlighting: **
-        $startpos = tell file_in;
-        next;
+    # Repeat the file for each datatype.
+    for (my $i=0; $i<@enumtypes; $i++) {
+	while (<file_in>) {
+	    if ($_ =~ /^\@begin_stringalso\s*.*/) { #*
+		$stringalso = 1;
+		next;
+	    }
+	    if ($_ =~ /^\@end_stringalso\s*.*/) { #*
+		$stringalso = 0;
+		next;
+	    }
+	    if ($enumtypes[$i] =~ NC_STRING and not $stringalso) { next; }
+	    $_ =~ s/\@nctype/$enumtypes[$i]/g;
+	    $_ =~ s/\@ctype/$ctypes[$i]/g;
+	    $_ =~ s/\@uctype/$uctypes[$i]/g;
+	    $_ =~ s/\@form/$formats[$i]/g;
+	    print file_out $_;
+	}
+	print file_out "\n";
+	seek(file_in, $startpos, 0);
     }
-    last;
-}
 
-$ntypes = @nctypes;
+    my $first = 1;
+    our $stringalso = 0;
 
-open my_out_wrappers, ">function_wrappers.c";
-$first = 1;
+    # Generate the function arrays and wrapper functions
+    while (<file_in>) {
+	if ($_ =~ /^\@begin_stringalso\s*.*/) { #*
+	    $stringalso = 1;
+	    next;
+	}
+	if ($_ =~ /^\@end_stringalso\s*.*/) { #*
+	    $stringalso = 0;
+	    next;
+	}
+	($type, $name, $args) = ($_ =~ /(\S+.*)\s+(\S+)_\@nctype*\((.*)\)\s*{/g); #+
 
-sub maybe_make_wrapper_function {
-    ($type, $name, $args) = ($_[0] =~ /(\S+.*)\s+(\S+)_\@nctype\S*\((.*)\)\s*{/g); # fix highlighting: +
-    if (!$type || !$name) {
-	return; }
-    if (substr($name, 0, 1) =~ '_') {
-	return; }
-    $type =~ s/^static\s+//;
-    if ($type =~ /^ctype\W*$/) {
-	return; }
-    # the dummy arguments must not be in the generated prototypes
-    # the substitution omits them and sets pointers (*) and commas (,) back
-    $args_dummy = $args;
-    $args =~ s/\s*(\**)\w+\s*(,|$)/$1$2/g;
-    if (!$first) {
-	print my_out_wrappers "\n"; }
-    $first = 0;
-    make_wrapper_function($type, $name, $args);
-    if (!$quiet) {
-	if ($hasvar) {
-	    print "$type $name($args_dummy);\n"; }
-	else {
-	    print "$type $name(nc_type, $args_dummy);\n"; }
+	if (!$type || !$name) { next; }
+	if (substr($name, 0, 1) =~ '_') { next; }
+	if ($type =~ /^ctype\W*$/) { next; }
+
+	# We later replace freely named arguments with _{number}.
+	# This substitution omits argument names them and sets pointers (*) and commas (,) back
+	$args =~ s/\s*(\**)\w+\s*(,|$)/$1$2/g;
+
+	if (not $first) {
+	    print file_out "\n"; }
+	$first = 0;
+
+	make_wrapper_function($type, $name, $args);
     }
+    close file_in;
 }
 
-for ($j=0; $j<$ntypes; $j++) {
-    $a = "autogenerated/$fname0";
-    $a =~ s/\.in\.c/_$nctypes[$j].c/;
-
-    open out2, ">$a"; # autogenerated/functions_NC_TYPE.c
-    seek file_in, $startpos, 0;
-    my $yes_wrapper = 1;
-    while(<file_in>) {
-	if ($_ =~ /\@start_no_wrapper$/) {
-	    $yes_wrapper = 0; }
-	elsif ($_ =~ /\@end_no_wrapper$/) {
-	    $yes_wrapper = 1; }
-	if ($j == 0 and $yes_wrapper) {
-	    maybe_make_wrapper_function($_); }
-
-	$_ =~ s/\@nctype/$nctypes[$j]/g;
-	$_ =~ s/\@form/$formats[$j]/g;
-	$_ =~ s/\@ctype/$ctypes[$j]/g;
-	$_ =~ s/\@uctype/$uctypes[$j]/g;
-	print out2 $_;
-    }
-    close out2;
-
-    print out1 "#include \"$a\"\n";
-}
-
-close my_out_wrappers;
-close file_in;
-
-print out1 "\n#include \"function_wrappers.c\"\n";
-close out1;
+main();
